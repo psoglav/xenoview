2906ee34c7787b864685da53886b906d73f09296
Andrey Nevinny <psoglav.ih8u@gmail.com>
2022-09-23 23:20:03 +0300

    feat(utils/math): add normalize function

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -2,4 +2,13 @@ export const lerp = (start: number, end: number, t: number) => {
   return start + (end - start) * t
 }
 
+export const normalize = (n: number, min: number, max: number): number =>
+  (n - min) / (max - min)
 
+export const normalizeTo = (
+  from: number,
+  fromMin: number,
+  fromMax: number,
+  toMin: number,
+  toMax: number,
+): number => (toMax - toMin) * normalize(from, fromMin, fromMax)
