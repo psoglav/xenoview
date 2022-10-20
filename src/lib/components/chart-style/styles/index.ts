115c8d965b5bd898f1f2ecdba5f955bf7eba5b0e
Andrey Nevinny <psoglav.ih8u@gmail.com>
2022-10-20 19:22:32 +0300

    feat(ChartStyle): cache styles on switching

--- src/lib/components/chart-style/styles/index.ts
+++ src/lib/components/chart-style/styles/index.ts
@@ -8,17 +8,26 @@ import { Candles, Line, Area, Bars, HollowCandles } from '.'
 import { Chart } from '@/lib/core'
 import { ChartStyle } from '@/lib/components'
 
+const CachedStyles: { [key: string]: ChartStyle } = {}
+
+const cacheStyle = (style: Chart.StyleName, instance: ChartStyle) => {
+  let cache = CachedStyles
+  if (!cache[style]) cache[style] = instance
+  return cache[style]
+}
+
 export function createChartStyle(chart: Chart): ChartStyle {
-  switch (chart.options.style) {
+  const style = chart.options.style
+  switch (style) {
     case 'candles':
-      return new Candles(chart)
+      return cacheStyle(style, new Candles(chart))
     case 'line':
-      return new Line(chart)
+      return cacheStyle(style, new Line(chart))
     case 'area':
-      return new Area(chart)
+      return cacheStyle(style, new Area(chart))
     case 'bars':
-      return new Bars(chart)
+      return cacheStyle(style, new Bars(chart))
     case 'hollow-candles':
-      return new HollowCandles(chart)
+      return cacheStyle(style, new HollowCandles(chart))
   }
 }
