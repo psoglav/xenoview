14ce3c8a681e4b267d351179204d842db8553a91
Andrey Nevinny <psoglav.ih8u@gmail.com>
2022-10-23 23:25:37 +0300

    chore: update build

--- dist/lib/core/chart.js
+++ dist/lib/core/chart.js
@@ -1,15 +1,12 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Chart = void 0;
 const components_1 = require("../components");
 const _1 = require(".");
 const chartStyle_1 = require("../components/chartStyle");
 require("../../public/styles/main.css");
-const moment_1 = __importDefault(require("moment"));
-console.log((0, moment_1.default)().format('HH:mm'));
+// import moment from 'moment'
+// console.log(moment().format('HH:mm'))
 const defaultChartOptions = {
     style: 'candles',
     bgColor: '#151924',
