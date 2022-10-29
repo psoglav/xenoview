c274a71000f7be0520b2c37bf465f163ad82d80b
Andrey Nevinny <psoglav.ih8u@gmail.com>
2022-10-30 01:26:44 +0300

    feat: create range model

---
+++ src/models/range.ts
@@ -0,0 +1,29 @@
+import { assert } from '@/utils/assertions'
+
+export default class Range<T extends number> {
+  private readonly _start: T
+  private readonly _end: T
+
+  public get start() {
+    return this._start
+  }
+
+  public get end() {
+    return this._end
+  }
+
+  constructor(start: T, end: T) {
+    assert(start < end, 'start should be less than end')
+
+    this._start = start
+    this._end = end
+  }
+
+  contains(value: number) {
+    return this.start >= value && value <= this.end
+  }
+
+  equals(other: Range<T>) {
+    return other.start === this.start && other.end === this.end
+  }
+}
